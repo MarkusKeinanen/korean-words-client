4e02683870704206051bf719226c546eee249205
Markus Keinänen <markusvk@protonmail.com>
2020-03-24 03:30:57 +0200

    asd

---
+++ docs/precache-manifest.32eb2c93fa4015fd4429a419a1801777.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95e09a300fd0751364e3f94f4cf1aa79",
+    "url": "/korean-words-client/index.html"
+  },
+  {
+    "revision": "f5a740a79a5f14441090",
+    "url": "/korean-words-client/static/css/main.8abfd859.chunk.css"
+  },
+  {
+    "revision": "5877191fd4da8b04027d",
+    "url": "/korean-words-client/static/js/2.1e75f79b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/korean-words-client/static/js/2.1e75f79b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5a740a79a5f14441090",
+    "url": "/korean-words-client/static/js/main.fdb739dc.chunk.js"
+  },
+  {
+    "revision": "f9c7ce63200539d2269f",
+    "url": "/korean-words-client/static/js/runtime-main.e94d4fa4.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b96241c64765e70aa775900f2de4f0db.js"
+  "/korean-words-client/precache-manifest.32eb2c93fa4015fd4429a419a1801777.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/korean-words-client/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
