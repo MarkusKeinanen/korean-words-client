c76b16e83050697b488181ec3c7f92f59864dd67
Markus Keinänen <markusvk@protonmail.com>
2020-03-24 03:39:39 +0200

    asd

---
+++ docs/precache-manifest.a7a8d7e13a4d65cab9dc7db35bd03937.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1417cfcd0118933c0e1f0b3f2d9b7c64",
+    "url": "/korean-words-client/index.html"
+  },
+  {
+    "revision": "6cf94697545c0142077f",
+    "url": "/korean-words-client/static/css/main.8abfd859.chunk.css"
+  },
+  {
+    "revision": "5877191fd4da8b04027d",
+    "url": "/korean-words-client/static/js/2.1e75f79b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/korean-words-client/static/js/2.1e75f79b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cf94697545c0142077f",
+    "url": "/korean-words-client/static/js/main.b75078ce.chunk.js"
+  },
+  {
+    "revision": "f9c7ce63200539d2269f",
+    "url": "/korean-words-client/static/js/runtime-main.e94d4fa4.js"
+  }
+]);
